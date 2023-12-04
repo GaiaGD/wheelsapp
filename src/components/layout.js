606aeaf5ea829f9dcd5a541ed29d3c0e9d870fbc
GaiaGD <gaia.digregorio@gmail.com>
2023-12-04 13:01:28 -0500

    framer motion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import { Work_Sans } from 'next/font/google'
 import { Inter } from 'next/font/google'
+import { motion } from "framer-motion";
 
 const work = Work_Sans({
     subsets: ['latin'],
@@ -18,13 +19,21 @@ const inter = Inter({
 export default function Layout({ children }) {
 
     return (
-        <main className={`h-screen ${inter.className} flex justify-center items-center`}>
-            <div className={`md:w-[600px] w-full`}>
-                <div>
-                    {children}
-                </div>
-            </div>
-        </main>
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.5 }}
+      >
+          <main className={`h-screen ${inter.className} flex justify-center items-center`}>
+              <div className={`md:w-[600px] w-full`}>
+                  <div>
+                      {children}
+                  </div>
+              </div>
+          </main>
+
+      </motion.div>
     )
 
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import '@/styles/globals.css'
+import { AnimatePresence } from 'framer-motion'
 import { AppContextProvider } from '@/context/context'
 
 export default function App({ Component, pageProps }) {
   return (
-    <AppContextProvider>
-      <Component {...pageProps} />
-    </AppContextProvider>
+      <AppContextProvider>
+        <AnimatePresence mode="wait" initial={false}>
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </AppContextProvider>
     )
 }
