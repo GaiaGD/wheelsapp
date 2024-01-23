8aac73bf59e6312c1cb0cd5d617b24acab8b4efc
GaiaGD <gaia.digregorio@gmail.com>
2024-01-22 21:18:57 -0500

    fix

--- src/pages/api/airlab-data.js
+++ src/pages/api/airlab-data.js
@@ -11,6 +11,3 @@ export default async function handler(req, res) {
     res.status(200).json({ airlabData })
   }
   
-
-  // https://airlabs.co/api/v9/flights?api_key=9d11dcc9-f80f-4372-a34c-63357d1db729&dep_iata=svo&arr_iata=pek&airline_iata=ca
-  // "9d11dcc9-f80f-4372-a34c-63357d1db729"
